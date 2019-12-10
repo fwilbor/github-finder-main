51c14e336234c0557bb6af46daf389dedec98dff
fwilbor <franklin.cfg@gmail.com>
2019-12-10 01:14:33 -0500

    add additional files and spinner.gif

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import spinner from '../layout/'
+
+export const Spinner = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Spinner
