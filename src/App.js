4cd248825f066b3cd68d3d505c29d6b0b8e22fa9
fwilbor <franklin.cfg@gmail.com>
2019-12-11 19:54:11 -0500

    made changes to App.js and got react-router working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -52,10 +53,9 @@ class App extends Component {
                 <Fragment>
                   <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} setAlert={this.setAlert} />
                   <Users loading={loading} users={users} />
-
                 </Fragment>
-
               )} />
+              <Route exact path='/about' component={About} />
             </Switch>
 
 
