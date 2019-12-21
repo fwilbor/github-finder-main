633aefd27026e6052830486178828d602983cc1f
fwilbor <franklin.cfg@gmail.com>
2019-12-20 20:42:00 -0500

    set context reducer and state

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -25,7 +25,14 @@ export default (state, action) => {
                 ...state,
                 users: [],
                 loading: false
+            };
+        case GET_REPOS: {
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
             }
+        }
         case SET_LOADING:
             return {
                 ...state,
